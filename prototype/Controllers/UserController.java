ae57ae1d480f2b7bf134616a77f54e12206745af
Dawson Zhang <dawson.zhang@umontreal.ca>
2024-07-06 12:41:17 -0400

    fix orders

--- prototype/Controllers/UserController.java
+++ prototype/Controllers/UserController.java
@@ -132,7 +132,7 @@ public class UserController {
                             if (component.getName().equalsIgnoreCase(supplierOrComponent[1].trim())){
                                 Order order = new Order(UUID.randomUUID(),Database.getTime(),Database.getTime().plusDays(5),new ArrayList<>(Arrays.asList(component)),"En cours", (Client) user,supplier);
                                 user.getOrders().add(order);
-                                //supplier.getOrders().add(order);
+                                supplier.getOrders().add(order);
                                 supplier.getStorage().remove(component);
                                 System.out.println("La commande a été passée avec succès");
                                 UserMenu.displayManageOrders(user);
