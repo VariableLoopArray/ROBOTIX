7609bbb49b58c34eca75436ae39c708d10effd87
dawson <dawson>
2024-07-18 21:01:36 -0400

    changed to email login

--- Devoir3/Robotix/src/main/java/Model/User.java
+++ Devoir3/Robotix/src/main/java/Model/User.java
@@ -48,5 +48,11 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
