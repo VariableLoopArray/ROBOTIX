e47dd69e10bf44ef682c2025fe886b6df4d58df8
OO <ouqiyun2021@gmail.com>
2024-07-19 15:33:52 -0400

    qiyun coding 6

--- Devoir3/Robotix/src/main/java/Controller/ActivityController.java
+++ Devoir3/Robotix/src/main/java/Controller/ActivityController.java
@@ -1,25 +1,15 @@
 package Controller;
 import Model.Activity;
-import Model.Robot;
+import Model.Task;
 import Model.TypeOfUsers.Client;
-import Model.User;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-
-import java.io.*;
-import java.util.Scanner;
-import java.util.UUID;
 
 public class ActivityController {
     public VBox DisplayActivities;
@@ -46,53 +36,88 @@ public class ActivityController {
 
     public void displayActivities(Client client){
         DisplayActivities.setSpacing(10);
+        Button buttonAdd = new Button("Add");
+        DisplayActivities.getChildren().add(buttonAdd);
         for (int i = 0; i < client.getMyActivities().size(); i++) {
+
             Activity activity = client.getMyActivities().get(i);
             Label newActivity = new Label(activity.getName());
             newActivity.getStyleClass().add("activities");
             HBox buttonBox = new HBox(10);
+
             Button buttonRemove = new Button("Remove");
-            buttonRemove.setOnAction(this::buttonRemove, i);
-            Button button2 = new Button("hello2");
-            Button button3 = new Button("hello3");
-            buttonBox.getChildren().addAll(buttonRemove, button2, button3);
+            int index = i;
+            buttonRemove.setOnAction((actionEvent -> buttonRemove(actionEvent, index)));
+
+            Button buttonModify = new Button("Modify");
+            buttonModify.setOnAction((actionEvent -> buttonModify(actionEvent, index)));
+
+            buttonBox.getChildren().addAll(buttonRemove, buttonModify);
             DisplayActivities.getChildren().add(newActivity);
             DisplayActivities.getChildren().add(buttonBox);
         }
 
     }
 
-    private void buttonRemove(ActionEvent event, int i) {
-        Button button = (Button) event.getSource();
-        HBox hbox = (HBox) button.getParent();
-        Label label = (Label) hbox.getParent().getChildrenUnmodifiable().get(0);
-        String activityName = label.getText();
-        client.getMyActivities().remove(activityName);
+    private void buttonModify(ActionEvent actionEvent, int index) {
+        Label modify = (Label) DisplayActivities.getChildren().get(2 * index + 1);
+        String modifyText = modify.getText();
 
-        for (Activity activity : client.getMyActivities()) {
-        System.out.println(activity.getName());
+        for (Activity modifyNode : client.getMyActivities()) {
+            if (modifyNode.getName().equals(modifyText)) {
+                TextArea newTask = new TextArea();
+                String tasks = "";
+                for (Task task : modifyNode.getTasks()) {
+                    tasks += task.getName() + "\n";
+                    for (String instructions : task.getInstructions()) {
+                        tasks += instructions + "\n";
+                        newTask.setText(tasks);
+                        DisplayActivities.getChildren().add(newTask);
+                    }
+                }
+                break;
+            }
         }
     }
 
-    public void activityGoBack() {
-        try {
-            Stage stage = (Stage) activityWelcome.getScene().getWindow();
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FxmlPages/HomepageMenu.fxml"));
-            Scene homepageMenu = new Scene(fxmlLoader.load(), 1024, 768);
-            homepageMenu.getStylesheets().remove(getClass().getResource("/CssFiles/Activity.css").toExternalForm());
-            homepageMenu.getStylesheets().add(getClass().getResource("/CssFiles/Homepage.css").toExternalForm());
-            stage.setTitle("Homepage");
-            HomepageController homepageController = fxmlLoader.getController();
-            homepageController.setClientHomepage(client);
-            stage.setScene(homepageMenu);
-            stage.show();
+            private void buttonRemove (ActionEvent event,int index){
 
 
-        } catch (Exception e) {
-            e.printStackTrace();
+                Label remove = (Label) DisplayActivities.getChildren().get(2 * index);
+                String removeText = remove.getText();
+
+                for (Activity removeNode : client.getMyActivities()) {
+                    if (removeNode.getName().equals(removeText)) {
+                        client.getMyActivities().remove(removeNode);
+                        break;
+                    }
+                }
+
+                for (Activity activity : client.getMyActivities()) {
+                    System.out.println(activity.getName());
+                }
+            }
+
+            public void activityGoBack () {
+                try {
+                    Stage stage = (Stage) activityWelcome.getScene().getWindow();
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FxmlPages/HomepageMenu.fxml"));
+                    Scene homepageMenu = new Scene(fxmlLoader.load(), 1024, 768);
+                    homepageMenu.getStylesheets().remove(getClass().getResource("/CssFiles/Activity.css").toExternalForm());
+                    homepageMenu.getStylesheets().add(getClass().getResource("/CssFiles/Homepage.css").toExternalForm());
+                    stage.setTitle("Homepage");
+                    HomepageController homepageController = fxmlLoader.getController();
+                    homepageController.setClientHomepage(client);
+                    stage.setScene(homepageMenu);
+                    stage.show();
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+            }
+
 
         }
-    }
 
-
-}
