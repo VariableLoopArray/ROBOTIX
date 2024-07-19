f4a12663c4e706d58345f762fbf0b48e08bedff9
dawson <dawson>
2024-07-18 23:47:46 -0400

    added profile

--- Devoir3/Robotix/src/main/java/Controller/HomepageController.java
+++ Devoir3/Robotix/src/main/java/Controller/HomepageController.java
@@ -3,8 +3,10 @@ package Controller;
 import javafx.fxml.FXML;
 
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,9 +17,17 @@ public class HomepageController {
     private Label messageLabel1;
     @FXML
     private Label messageLabel2;
+    @FXML
+    private HBox menuProfile;
 
     public HomepageController() {
     }
+    @FXML
+    public void initialize(){
+        menuProfile.alignmentProperty().setValue(javafx.geometry.Pos.CENTER);
+        menuProfile.setPadding(new Insets(10, 10, 10, 10));
+    }
+
     public void displayMessage(String message, boolean isError) {
         if (!isError) {
             messageLabel1.setText(message);
@@ -25,10 +35,27 @@ public class HomepageController {
             messageLabel2.setText(message);
         }
     }
+    public void goToMyProfile(){
+        try {
+            Stage stage = (Stage) messageLabel1.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FxmlPages/ProfileMenu.fxml"));
+            Scene profileScene = new Scene(fxmlLoader.load(), 1920, 1080);
+            profileScene.getStylesheets().remove(getClass().getResource("/CssFiles/Homepage.css").toExternalForm());
+            profileScene.getStylesheets().add(getClass().getResource("/CssFiles/Profile.css").toExternalForm());
+            ProfileController profileController = fxmlLoader.getController();
+            stage.setTitle("My Profile");
+            stage.setScene(profileScene);
+            stage.show();
+            stage.setFullScreen(true);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void goToActivity() throws IOException {
         try {
             Stage stage = (Stage) messageLabel1.getScene().getWindow();
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FxmlPages/Activity.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FxmlPages/ActivityMenu.fxml"));
             Scene activityScene = new Scene(fxmlLoader.load(), 1920, 1080);
             activityScene.getStylesheets().remove(getClass().getResource("/CssFiles/Homepage.css").toExternalForm());
             activityScene.getStylesheets().add(getClass().getResource("/CssFiles/Activity.css").toExternalForm());
---
+++ Devoir3/Robotix/src/main/java/Controller/ProfileController.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class ProfileController {
+    @FXML
+    private Label profileMenu;
+    public ProfileController() {
+    }
+    @FXML
+    public void initialize() {
+
+    }
+    public void handleGoBack() {
+        try {
+            Stage stage = (Stage) profileMenu.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FxmlPages/HomepageMenu.fxml"));
+            Scene homepage = new Scene(fxmlLoader.load(), 1920, 1080);
+            homepage.getStylesheets().remove(getClass().getResource("/CssFiles/Profile.css").toExternalForm());
+            homepage.getStylesheets().add(getClass().getResource("/CssFiles/Homepage.css").toExternalForm());
+            stage.setTitle("Homepage");
+            stage.setScene(homepage);
+            stage.show();
+            stage.setFullScreen(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+}
