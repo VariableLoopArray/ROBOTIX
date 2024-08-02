110ed0da34b1fc3a1d178634e31f2ccd36cbe86f
OO <ouqiyun2021@gmail.com>
2024-08-01 23:14:31 -0400

    Qiyun RobotMenu5

--- Devoir3/Robotix/src/main/java/Controller/RobotController.java
+++ Devoir3/Robotix/src/main/java/Controller/RobotController.java
@@ -24,8 +24,6 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class RobotController {
-//    @FXML
-//    public VBox DisplayRobots;
     @FXML
     public Label RobotWelcome;
     @FXML
@@ -62,52 +60,56 @@ public class RobotController {
 
     Client client;
 
-//    public void initialize() {
-//        System.out.println("ActivityController initialized: " + (activityWelcome != null));
-//    }
-
     public void displayMessage(String message, boolean isError) {
         //RobotWelcome.getStylesheets(Message);
         if (!isError) {
             RobotWelcome.setText(message);
+            RobotWelcome.getStyleClass().add("default-text");
+            RobotWelcome.getStyleClass().remove("error-text");
         } else {
             RobotWelcome.setText("Error: " + message);
+            RobotWelcome.getStyleClass().add("error-text");
+            RobotWelcome.getStyleClass().remove("default-text");
         }
     }
     public void setUserRobot(Client client){
         this.client = client;
         displayMessage("Welcome to your robots!", false);
     }
-
-
-    public void displayRobots(Client client){
-/*      if (client.getFleet().size() == 0){
-            displayMessage("You have no robots!", false);
-        } else {
-            displayMessage("Here are your robots!", false);
-        }*/
-/*        Button creat = new Button("Create My Robot");
-        GridPane grid = new GridPane();
-        DisplayRobots.getChildren().add(creat);
-        grid.add(creat, 0, 0);
-        grid.setAlignment(Pos.CENTER);*/
-    }
-
-
-    public void welcomePage(ActionEvent actionEvent) {
+    public void showRobot(ActionEvent actionEvent) {
         Label robotlist = new Label();
+        robotlist.getStyleClass().add("label-robotlist");
+        DisplayRobots.getChildren().add(robotlist);
         afficherRobot(actionEvent,robotlist);
-        create.setAlignment(Pos.CENTER);
-        affiche.setAlignment(Pos.CENTER);
+
+/*      GridPane gridPane = new GridPane();
+        gridPane.add(robotlist, 0, 0);
+        GridPane.setHAlignment(robotlist, HPos.CENTER); // 设置水平对齐方式
+        gridPane.setAlignment(Pos.CENTER);*/
+        //create.setAlignment(Pos.CENTER);
+        //affiche.setAlignment(Pos.CENTER);
     }
 
     public void afficherRobot(ActionEvent actionEvent,Label robotlist) {
+        affiche.setVisible(false);
+        affiche.setManaged(false);
+        create.setVisible(true);
+        create.setManaged(true);
+
         String robot = "";
+        Label norobot = new Label("You have no robot!");
         if(client.getFleet().size() == 0){
-            robotlist.setText("You have no robots!");
-            DisplayRobots.getChildren().add(robotlist);
+            //robotlist.setText("You have no robot!");
+            norobot.getStyleClass().add("label-no-robots");
+            DisplayRobots.getChildren().add(norobot);
             return;
         }
+        else{
+            norobot.setVisible(false);
+            norobot.setManaged(false);
+            //DisplayRobots.getChildren().remove(norobot);
+        }
+
         try(Reader reader = new FileReader("src/main/JsonFiles/client.json")){
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             Client [] Clients = gson.fromJson(reader, Client[].class);
@@ -131,6 +133,8 @@ public class RobotController {
             }
             robotlist.setText(robot);
             DisplayRobots.getChildren().add(robotlist);
+
+
         }
         catch (IOException ec){
             ec.printStackTrace();
@@ -139,6 +143,7 @@ public class RobotController {
 
 
     public void confirmRobot(ActionEvent actionEvent,Label robotlist) {
+        // 点击确认按钮后，把之前已经显示的robot列表删除，robotlist是否要对全局应用 ！！（当列表为空时不显示内容，正常时候会一直显示）
         Robot robot = new Robot(name.getText(), type.getText(),new ArrayList<Component>(), battery.getText(),
                 new float[] {0.0f, 0.0f, 0.0f}, Float.valueOf(speed.getText()),Float.valueOf(cpuusage.getText()),Float.valueOf(memory.getText()));
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -146,10 +151,10 @@ public class RobotController {
         System.out.println(name.getText());
         Informations.setVisible(false);
         Label success = new Label("Robot created successfully!");
+        success.getStyleClass().add("label-success");
         DisplayRobots.getChildren().add(success);
 
         try(Reader reader = new FileReader("src/main/JsonFiles/client.json")){
-
             Client [] Clients = gson.fromJson(reader, Client[].class);
             for (int i = 0; i < Clients.length; i++){
                 if (Clients[i].getId().equals(client.getId())){
@@ -164,91 +169,54 @@ public class RobotController {
         catch (IOException ec){
             ec.printStackTrace();
         }
-        Button afficher = new Button("Show My Robots");
+/*        Button afficher = new Button("Show My Robots");
+        afficher.getStyleClass().add("button-show");
         afficher.setOnAction(e -> afficherRobot(e,robotlist));
-        DisplayRobots.getChildren().add(afficher);
+        DisplayRobots.getChildren().add(afficher);*/
+
+        success.setManaged(true); //点击确认后显示添加成功
+        affiche.setManaged(true);
+        affiche.setVisible(true);
+        // TODO需显示showlist按钮！！！！
+        //success.setManaged(false); TODO并更改文字为显示"Here is your robot list"的label
+
+        //afficherRobot(actionEvent,robotlist);
+
     }
     public void creatRobot(ActionEvent actionEvent) {
-
+        //.clear();
+        affiche.setManaged(false); // 尝试当点击create时隐藏showlist按钮
+        affiche.setVisible(false); // 并隐藏robot列表
         Label robotlist = new Label();
-        Informations.setVisible(true);
-        create.setVisible(false);
-        create.setManaged(false);
+        Informations.setVisible(true); // Show the informations
+        create.setVisible(false); // Hide the create button
+        create.setManaged(false); // Hide the create button
+/*        affiche.setManaged(false); // Hide the affiche button
+        affiche.setVisible(false); // Hide the affiche button  无用 */
         Button confirm = new Button("Confirm");
+        confirm.getStyleClass().add("button-confirm");
         confirm.setOnAction(e -> confirmRobot(e,robotlist));
         Informations.getChildren().add(confirm);
         Informations.setRowIndex(confirm, 9);
         Informations.setColumnIndex(confirm, 1);
 
+        //afficherRobot(actionEvent,robotlist); 不可用，每点一次create就重新显示一次列表
+
+        String robot = "";
+        Label norobot = new Label("You have no robot!");
+        if(client.getFleet().size() == 0){
+            //robotlist.setText("You have no robot!");
+//            norobot.getStyleClass().add("label-no-robots");
+//            DisplayRobots.getChildren().add(norobot);
+            norobot.setVisible(false);
+            norobot.setManaged(false);
+        }
+        else{
+            norobot.setVisible(false);
+            norobot.setManaged(false);
+            DisplayRobots.getChildren().remove(norobot);
+        }
     }
-
-/*    private void welcome(ActionEvent action) {
-        Button createMyRobots = new Button("Create My Robots");
-        createMyRobots.setOnAction(e -> creatRobot(e));
-        DisplayRobots.getChildren().add(createMyRobots);
-
-
-        Label robotlist = new Label();
-        Button afficher = new Button("Show My Robots");
-        afficher.setOnAction(e -> afficherRobot(e,robotlist));
-        DisplayRobots.getChildren().add(afficher);
-    }*/
-/*        DisplayRobots.setSpacing(10);
-//        Button buttonAdd = new Button("Add");
-//        DisplayActivities.getChildren().add(buttonAdd);
-        AtomicInteger numberOfActivity = new AtomicInteger();
-        ArrayList<Integer> numbersRemoved = new ArrayList<Integer>();
-        for (int i = 0; i < client.getMyActivities().size(); i++) {
-
-            VBox everything = new VBox(10);
-            Robot robot = client.getFleet().get(i);
-            //VBox activityAndModify = new VBox(1);
-            Label newRobot = new Label(robot.getName());
-
-            //activityAndModify.getChildren().addAll(newActivity, newTask);
-
-            newRobot.getStyleClass().add("activities");
-
-            HBox buttonBox = new HBox(5);
-
-
-            DisplayRobots.getChildren().add(everything);
-
-            //DisplayActivities.getChildren().add(buttonBox);
-        }
-        if (client.getFleet().size() == 0){
-            noRobotList.setText("You have no activities");
-        }
-
-    }*/
-
-/*    public void createRobot() {
-        try {
-            ArrayList<String> robots = new ArrayList<String>();
-            for (int i = 1; i <= 10; i++) {
-                CheckBox robot = (CheckBox) getClass().getDeclaredField("Robot" + i).get(this);
-                if (robot.isSelected()) {
-                    robots.add(robot.getText());
-                }
-            }
-
-
-            Robot newRobot = new Robot(robotName.getText(),type.getText(), new ArrayList<Component>(),
-                    battery.getText(), new float[]{0.0f, 0.0f, 0.0f},Float.valueOf(speed.getText()),Float.valueOf(cpuUsage.getText()),Float.valueOf(memory.getText()));
-
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String json = gson.toJson(newRobot);
-            try (Writer writer = new FileWriter("src/main/JsonFiles/activities.json")) {
-                writer.write(json);
-            }
-
-
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException | IOException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
 
 
     public void handleGoBack() {
@@ -263,18 +231,9 @@ public class RobotController {
             homepageController.setUserHomepage(client);
             stage.setScene(homepageMenu);
             stage.show();
-
-
         } catch (Exception e) {
             e.printStackTrace();
 
         }
     }
-
-
-
-
-/*
-    public void setUserActivity(Client client) {
-    }*/
 }
