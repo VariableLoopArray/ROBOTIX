40911d7a6032ca81eabecedd388b736d911a3b6d
dawson <zhanda4@gmail.com>
2024-08-05 20:21:29 -0400

    retry Actions

--- Application/Robotix/src/main/java/module-info.java
+++ Application/Robotix/src/main/java/module-info.java
@@ -1,16 +1,14 @@
 module main.Robotix {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
     requires com.google.gson;
-    requires org.junit.jupiter.api;
 
     opens Model;
-    opens Controller to javafx.fxml, org.junit.jupiter.api;
+    opens Controller to javafx.fxml;
     opens main to javafx.fxml;
     opens Model.TypeOfUsers;
 
