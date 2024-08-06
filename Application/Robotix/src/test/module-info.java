06b3a753b21d6a53bee00c829f9eb1c4a00a70c6
dawson <zhanda4@gmail.com>
2024-08-06 01:29:56 -0400

    retry

---
+++ Application/Robotix/src/test/module-info.java
@@ -0,0 +1,6 @@
+module Robotix {
+    requires main.Robotix; // Requires your main module
+    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.engine;
+    // Additional requires if necessary
+}
